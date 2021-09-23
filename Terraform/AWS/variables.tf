81eca2fe741d7f4e2c06429506d9a7d00ad62321
techielins <68058598+techielins@users.noreply.github.com>
2021-09-23 22:11:05 +0530

    Create variables.tf

---
+++ Terraform/AWS/variables.tf
@@ -0,0 +1,15 @@
+ariable aws_region{
+    default = "us-east-1"
+}
+variable vpc_cidr_block{
+    default = "172.10.0.0/16"
+}
+variable aws_az{
+    default = "us-east-1a"
+}
+variable aws_ami{
+    default = "ami-0dc2d3e4c0f9ebd18"
+}
+variable aws_key_name{
+    default = "EC2KeyPairs"
+}
