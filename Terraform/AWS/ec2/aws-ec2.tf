6a73d5cde6dbba74675e1cacc7799c7b296771d1
techielins <68058598+techielins@users.noreply.github.com>
2021-09-27 17:01:19 +0530

    Update aws-ec2.tf

--- Terraform/AWS/ec2/aws-ec2.tf
+++ Terraform/AWS/ec2/aws-ec2.tf
@@ -1,43 +1,32 @@
-resource "aws_vpc" "my_vpc" { 
-  cidr_block = var.vpc_cidr_block
-}
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.vpc_cidr_block
-  availability_zone = var.aws_az
-}
-resource "aws_security_group" "my_sg" {
-  name   = "HTTP and SSH"
-  vpc_id = aws_vpc.my_vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 resource "aws_instance" "myInstance" {
-  ami           = var.aws_ami
-  instance_type = "t2.micro"
-  key_name      = var.aws_key_name
-  subnet_id     = aws_subnet.my_subnet.id
-
+  ami                           = var.aws_ami
+  instance_type                 = "t2.micro"
+  availability_zone             = var.aws_az
+  key_name                      = var.aws_key_name
+  subnet_id                     = aws_subnet.subnet_name.id
+  ebs_optimized                 = false
+  hibernation                   = false
+  instance_type                 = var.instance_type
+  private_ip                    = var.private_ip
+  monitoring                    = false
+  secondary_private_ips         = []
+  security_groups               = []
+  source_dest_check             = true 
   tags = {
     Name = "linuxhost01"
   }
+  
+  credit_specification {
+        cpu_credits = "standard"
+    }
+  
+  root_block_device {
+        delete_on_termination = true
+        encrypted             = false
+        iops                  = 100
+        volume_size           = 8
+    }
+
+    timeouts {}
 }
