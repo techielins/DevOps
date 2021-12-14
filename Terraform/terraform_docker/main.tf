9fe66daec9abc5e1dd650734f4849674fb05b124
Linson Joseph <68058598+techielins@users.noreply.github.com>
2021-12-14 17:05:09 +0530

    Update main.tf

--- Terraform/terraform_docker/main.tf
+++ Terraform/terraform_docker/main.tf
@@ -16,23 +16,62 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random1" {
+  length           = 4
+  special          = false
+  upper             = false
+  
+}
+
+
+resource "random_string" "random2" {
+  length           = 4
+  special          = false
+  upper             = false
+  
+}
+
+
 # Start a container
-resource "docker_container" "nodered_container" {
-  name  = "nodered"
+resource "docker_container" "nodered_container1" {
+  name  = join("-",["nodered",random_string.random1.result])
   image = docker_image.nodered_image.latest
   
   ports {
     internal = 1880
-    external = 1880
+    #external = 1880
   }
 }
 
-output "container_ip_addr" {
-  value       = join(":", [docker_container.nodered_container.ip_address,docker_container.nodered_container.ports[0].external])
-  description = "The private IP address and External port of the container."
+resource "docker_container" "nodered_container2" {
+  name  = join("-",["nodered",random_string.random2.result])
+  image = docker_image.nodered_image.latest
+  
+  ports {
+    internal = 1880
+    #external = 1880
+  }
 }
 
-output "container_name" {
-  value       = docker_container.nodered_container.name
-  description = "The name of the container."
+output "container_ip_addr1" {
+  value       = join(":",[docker_container.nodered_container1.ip_address,docker_container.nodered_container1.ports[0].external])
+  description = "The private IP address and External port of the container1."
 }
+
+output "container_name1" {
+  value       = docker_container.nodered_container1.name
+  description = "The name of the container1."
+  
+}
+
+output "container_ip_addr2" {
+  value       = join(":",[docker_container.nodered_container2.ip_address,docker_container.nodered_container2.ports[0].external])
+  description = "The private IP address and External port of the container2."
+}
+
+output "container_name2" {
+  value       = docker_container.nodered_container2.name
+  description = "The name of the container2."
+  
+}
+
