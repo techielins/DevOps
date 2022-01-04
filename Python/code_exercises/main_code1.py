c5fdb093690258ee4844dfbe17f038438bdecba7
Linson Joseph <68058598+techielins@users.noreply.github.com>
2022-01-04 17:09:46 +0530

    Update main_code1.py

---
+++ Python/code_exercises/main_code1.py
@@ -0,0 +1,5 @@
+#Write your code below this line 👇
+#print("Hello" + " " + input("What is your name\n"))
+name = input("What is your name?\n")
+#print(len(input("What is your name?\n")))
+print(len(name))
