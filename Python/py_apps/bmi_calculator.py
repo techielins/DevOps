4901eb107943ed557ae5f52fa4f295618d0e099c
Linson Joseph <68058598+techielins@users.noreply.github.com>
2022-01-10 17:03:25 +0530

    Create bmi_calculator.py

---
+++ Python/py_apps/bmi_calculator.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+result = int(weight)/float(height)**2
+#convert to int value
+bmi = int(result)
+#print(type(bmi))
+#print output
+print(bmi)
