735838f6e5c8a3c2e653426d2ac8776fab587c76
Linson Joseph <68058598+techielins@users.noreply.github.com>
2022-01-18 16:36:44 +0530

    Update tip_calculator.py

--- Python/py_apps/tip_calculator.py
+++ Python/py_apps/tip_calculator.py
@@ -7,8 +7,8 @@
 
 #Provide the input
 print("Welcome to the tip calculator.")
-bill = (input("What was the total bill? $"))
-tip = (input("What percentage tip would you like to give? 10, 12, or 15? "))
+bill = input("What was the total bill? $")
+tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
 split_no = input("How many people to split the bill? ")
 
 #Calculate the pay for each person
