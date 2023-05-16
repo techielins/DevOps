6100c4fca6300f9f0c0a34d47c9853f70400fb77
Linson Joseph <68058598+techielins@users.noreply.github.com>
2023-05-16 20:18:07 +0530

    Update variables.tf

--- Terraform/AWS/aurora-psql/variables.tf
+++ Terraform/AWS/aurora-psql/variables.tf
@@ -154,11 +154,6 @@ variable "replica_count" {
 #SECURITY GROUP
 ##############################################################
 
-variable "vpc_id"{
-    description = "The VPC id to which the security groups are mapped."
-    type        = string
-}
-
 variable "create_security_group"{
     description = "Specifies whether a security group should be created or not"
                     //If false, then existing security group is used for creating the DB cluster
