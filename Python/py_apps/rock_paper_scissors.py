72e1b78b05cd201d00be273c6814bfc97d8e7c52
techielins <linson.aj@gmail.com>
2023-09-19 21:11:31 +0530

    Adding rock-paper-scissors

--- Python/py_apps/rock_paper_scissors.py
+++ Python/py_apps/rock_paper_scissors.py
@@ -3,33 +3,41 @@ user_wins = 0
 computer_wins = 0
 options = ["rock", "paper", "scissors"]
 
-while True:
-  user_input = input("Enter Rock/Paper/Scissors :").lower()
-  print(f"You chose {user_input}")
+while True: 
+    
+  user_input = input("Enter Rock/Paper/Scissors or Q to quit:").lower()
+
+  if user_input == "q":
+     break
+       
   if user_input not in options:
-      print("You choose wrong choice and program will exit here.")
-      print(f"You won {user_wins} times") 
-      print(f"Computer won {computer_wins}")
-      print("Quit")
-      break
+     print("Please enter valid input.") 
+     continue
+  
   random_number = random.randint(0,2)
   # rock = 0 , paper = 1, scissors = 2
   computer_input = options[random_number]
   print(f"Computer chose {computer_input}")
+  
   if user_input == "rock" and computer_input == "scissors":
-      print("You won")
+      print("You won!")
       user_wins +=1
-      continue
-  if user_input == "paper" and computer_input == "rock":
-      print("You won")
+ 
+  elif user_input == "paper" and computer_input == "rock":
+      print("You won!")
       user_wins +=1
-      continue
-  if user_input == "scissors" and computer_input == "paper":
-      print("You won")
+
+  elif user_input == "scissors" and computer_input == "paper":
+      print("You won!")
       user_wins +=1
+      
   else:
-      print("Computer won")   
+      print("Computer won!")   
       computer_wins +=1 
 
-                 
+print("You have decided to quit.")
+print("Here is the result") 
+print(f"You won {user_wins} times") 
+print(f"Computer won {computer_wins} times") 
+print("Thanks for playing. Good Bye!")                  
  
